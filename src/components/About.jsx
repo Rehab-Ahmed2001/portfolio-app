32c8e6982d66a6e479866b8e968aef3c4f658d5e
Rehab Ahmed <khawasrehab@gmail.com>
2026-01-24 16:57:36 +0200

    Added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,14 +1,31 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
-    return (
-        <div className="mt-6 bg-white p-6 rounded-xl shadow-md">
-            <h2 className="text-xl font-bold text-gray-900 mb-2">About Me</h2>
-            <p className="text-gray-700 leading-relaxed">
-                I'm Rehab Ahmed Khawas, a recent graduate from the Faculty of Navigation Science and Space Technology, majoring in Space Navigation.
-                I am a Frontend Developer skilled in HTML5, CSS3, JavaScript (ES6+), TypeScript, React.js, and Next.js, with experience in building responsive user interfaces using Tailwind CSS, shadcn/ui, and Bootstrap, managing application state with React Hooks, Context API, and Redux (Basics), working with RESTful APIs and JSON, and utilizing Git, GitHub, VS Code, npm, and Node.js (Basics), along with strong problem-solving, debugging, communication, teamwork, and time management skills.
-                I’m always eager to learn, grow, and contribute to meaningful tech projects. Let’s connect!
-            </p>
-        </div>
-    )
+  return (
+    <div className="mt-6 bg-white p-6 rounded-xl shadow-md">
+      <h2 className="text-xl font-bold text-gray-900 mb-2">
+        About Me
+      </h2>
+
+      <p className="
+        text-gray-700 
+        leading-relaxed 
+        text-justify
+        break-words
+        hyphens-auto
+      ">
+        I'm Rehab Ahmed Khawas, a recent graduate from the Faculty of Navigation
+        Science and Space Technology, majoring in Space Navigation. I am a
+        Frontend Developer skilled in HTML5, CSS3, JavaScript (ES6+),
+        TypeScript, React.js, and Next.js, with experience in building
+        responsive user interfaces using Tailwind CSS, shadcn/ui, and
+        Bootstrap, managing application state with React Hooks, Context API,
+        and Redux (Basics), working with RESTful APIs and JSON, and utilizing
+        Git, GitHub, VS Code, npm, and Node.js (Basics), along with strong
+        problem-solving, debugging, communication, teamwork, and time
+        management skills. I’m always eager to learn, grow, and contribute to
+        meaningful tech projects. Let’s connect!
+      </p>
+    </div>
+  );
 }
